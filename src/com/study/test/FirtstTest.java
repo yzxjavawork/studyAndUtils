24dd3c13c5189c772ff57ae6a4e260c4783a3a63
yzxjavawork <yzxjavawork@163.com>
2016-05-04 12:41:05 +0800

    第一次测试

--- src/com/study/test/FirtstTest.java
+++ src/com/study/test/FirtstTest.java
@@ -1,9 +1,21 @@
 package com.study.test;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+import org.junit.Test;
+
 public class FirtstTest {
 	
-	
+	@Test
 	public void test(){
+//		查看其他线程的试用情况
+		ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+		ThreadInfo[] dumpAllThreads = threadMXBean.dumpAllThreads(false, false);
+		for (ThreadInfo threadInfo : dumpAllThreads) {
+			System.out.println("【" + threadInfo.getThreadId()+"】 " +threadInfo.getThreadName());
+		}
 		
 	}
 
